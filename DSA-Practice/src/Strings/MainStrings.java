f4d5bbaeab67e1ba6d8307330a6544b93bafadb1
tobezero01 <ducnhuad@gmail.com>
2025-07-09 18:09:03 +0700

    add new dsa

--- DSA-Practice/src/Strings/MainStrings.java
+++ DSA-Practice/src/Strings/MainStrings.java
@@ -45,6 +45,68 @@ public class MainStrings {
         }
         return right - left - 1;
     }
+
+    // 7. Reverse Integer
+    public static int Reverse(int x) {
+        int res = 0 ;
+        boolean isNegative = 0 < x;
+        String strX = String.valueOf(Math.abs(x));
+        StringBuilder sb = new StringBuilder(strX).reverse();
+
+        try {
+            res = Integer.parseInt(sb.toString());
+        } catch (NumberFormatException ex) {
+            return 0;
+        }
+        return isNegative ? -res : res;
+    }
+
+    //8. String to Integer (atoi)
+    public static int myAtoi(String s) {
+        if (s == null || s.isEmpty()) return 0;
+        int index = 0;
+        int n = s.length();
+
+        // bo qua khoang trang
+        while (index < n && s.charAt(index) == ' ') {
+            index++;
+        }
+
+        // neu het chuoi
+        if (index == n) return 0;
+
+        //xac dinh dau
+        int sign = 1;
+        char tempChar = s.charAt(index);
+        if (tempChar == '+' || tempChar == '-') {
+            if (tempChar == '-') sign = -1;
+            index++;
+        }
+
+        long res = 0; // chuoi co the rat dai
+        while (index < n && s.charAt(index) == '0') {
+            index++;
+        }
+
+        while (index < n) {
+            tempChar = s.charAt(index);
+            if (tempChar < '0' || tempChar > '9') break;
+
+            int digit = tempChar - '0';
+            res = res*10 + digit;
+
+            long signedRes = res * sign;
+            if (signedRes <= Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+            if (signedRes >= Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+            index++;
+        }
+        res *= sign;
+        return (int) res;
+    }
     public static void main(String[] args) {
 
     }
